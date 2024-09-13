d1002281b445061b5c6cd718f26f67ddcac55da5
stefan <marlowsoprano1234@gmail.com>
2024-09-13 14:41:38 +0200

    JSON Website implementiert

--- src/main/java/com/academy/spring_project/GreetingController.java
+++ src/main/java/com/academy/spring_project/GreetingController.java
@@ -3,13 +3,29 @@ package com.academy.spring_project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(Model model) {
-        //model.addAttribute("Name", "World");
+    public String greeting(
+        @RequestParam(name="n", required = false, defaultValue = "Welt!!")
+        String someName, Model model) {
+
+        model.addAttribute("inputName", someName);
         return "greeting";
+    }
+
+    @GetMapping("/person")
+    @ResponseBody
+    public Person personJson(){
+
+        Person person = new Person();
+        person.setId(1);
+        person.setName("Max");
+
+        return person;
     }
 }
--- src/main/java/com/academy/spring_project/Person.java
+++ src/main/java/com/academy/spring_project/Person.java
@@ -1,4 +1,24 @@
 package com.academy.spring_project;
 
-public class person {
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Person {
+    private int id;
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setName (String name){
+            this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
